c49ea194c885a095554a10e3fc1b12834d3ed7f0
ludo05 <lewiswilliams1501@gmail.com>
2023-04-07 16:28:10 +0000

    updated Dockerfile

---
+++ src/main/java/Lumaths.java
@@ -0,0 +1,10 @@
+public class Lumaths {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a -  b;
+    }
+}
